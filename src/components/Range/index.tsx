3f4ef875e4983068c4ef8f7917c9b91074fa4881
blopez-dev <pjgl.frontend@gmail.com>
2024-04-15 18:56:36 +0200

    Added new functions in custom hook to contol slides and manual editing of values

--- src/components/Range/index.tsx
+++ src/components/Range/index.tsx
@@ -6,23 +6,68 @@ import { useCustomRange } from '@/hooks/useCustomRange'
 import { positionFormatter } from '@/services/positionFormater'
 
 export default function Range({ minValueRange, maxValueRange }: TRange) {
-  const { minValue, maxValue } = useCustomRange(minValueRange, maxValueRange)
+  const {
+    minValue,
+    maxValue,
+    isEditingMinValue,
+    isEditingMaxValue,
+    handleSlidingValue,
+    setEditingMinvalue,
+    setEditingMaxValue,
+    handleValueSubmit
+  } = useCustomRange(minValueRange, maxValueRange)
   const { minRangePosition, maxRangePosition } = positionFormatter(minValue, maxValue, minValueRange, maxValueRange)
   const pointerWidth = 15
 
   return (
     <section className={styles.rangeContainer}>
-      <label className={styles.labelValue}>
-        <span>0</span>
-      </label>
+      {isEditingMinValue ? (
+        <input
+          type='number'
+          defaultValue={minValue}
+          onBlur={e => handleValueSubmit(e.currentTarget.value, true)}
+          onKeyUp={e => {
+            e.key === 'Enter' ? (e.currentTarget as HTMLInputElement).blur() : null
+          }}
+          onFocus={e => e.target.select()}
+          pattern='[0-9]*'
+          autoFocus
+        />
+      ) : (
+        <label className={styles.labelValue} onClick={() => setEditingMinvalue(true)}>
+          <span>{minValue}</span>
+        </label>
+      )}
       <div className={styles.slideRange}>
         <div className={styles.slideTrack} />
-        <div className={styles.pointer} style={{ left: `calc(${minRangePosition}% - ${pointerWidth / 2}px)` }}></div>
-        <div className={styles.pointer} style={{ left: `calc(${maxRangePosition}% - ${pointerWidth / 2}px)` }}></div>
+        <div
+          className={styles.pointer}
+          style={{ left: `calc(${minRangePosition}% - ${pointerWidth / 2}px)` }}
+          onMouseDown={e => handleSlidingValue(e, true)}
+        ></div>
+        <div
+          role='slider'
+          className={styles.pointer}
+          style={{ left: `calc(${maxRangePosition}% - ${pointerWidth / 2}px)` }}
+          onMouseDown={e => handleSlidingValue(e, false)}
+        ></div>
       </div>
-      <label className={styles.labelValue}>
-        <span>200</span>
-      </label>
+      {isEditingMaxValue ? (
+        <input
+          type='number'
+          defaultValue={maxValue}
+          onBlur={e => handleValueSubmit(e.currentTarget.value, false)}
+          onKeyUp={e => {
+            e.key === 'Enter' ? (e.currentTarget as HTMLInputElement).blur() : null
+          }}
+          onFocus={e => e.target.select()}
+          autoFocus
+        />
+      ) : (
+        <label className={styles.labelValue} onClick={() => setEditingMaxValue(true)}>
+          <span>{maxValue}</span>
+        </label>
+      )}
     </section>
   )
 }
--- src/hooks/useCustomRange.ts
+++ src/hooks/useCustomRange.ts
@@ -1,11 +1,67 @@
 import { useState } from 'react'
+import { act } from 'react-dom/test-utils'
+import { is } from 'immutable'
 
 export function useCustomRange(initialMin: number, initialMax: number) {
   const [minValue, setMinValue] = useState(initialMin)
   const [maxValue, setMaxValue] = useState(initialMax)
+  const [isEditingMinValue, setEditingMinvalue] = useState(false)
+  const [isEditingMaxValue, setEditingMaxValue] = useState(false)
+  const updateValues = (newValue: number, isMinValue: boolean) => {
+    if (isMinValue) {
+      if (newValue >= initialMin && newValue <= maxValue) {
+        setMinValue(newValue)
+      }
+    } else {
+      if (newValue >= minValue && newValue <= initialMax) {
+        setMaxValue(newValue)
+      }
+    }
+  }
+  const handleSlidingValue = (e: React.MouseEvent<HTMLElement>, isMinThumb: boolean) => {
+    e.preventDefault()
+    const actualThumbPosition = e.currentTarget
+    console.log(actualThumbPosition)
+    if (!actualThumbPosition.parentElement) return
 
+    const { left, width } = actualThumbPosition.parentElement.getBoundingClientRect()
+    const onSlide = (moveMouseEvent: MouseEvent) => {
+      const newThumbPosition = initialMin + ((moveMouseEvent.clientX - left) / width) * (initialMax - initialMin)
+      if (isMinThumb) {
+        if (Math.round(newThumbPosition) < maxValue) {
+          updateValues(Math.round(newThumbPosition), true)
+        }
+      } else {
+        if (Math.round(newThumbPosition) > minValue) {
+          updateValues(Math.round(newThumbPosition), false)
+        }
+      }
+    }
+    const onStopSlide = () => {
+      document.removeEventListener('mousemove', onSlide)
+      document.removeEventListener('mouseup', onStopSlide)
+    }
+    document.addEventListener('mousemove', onSlide)
+    document.addEventListener('mouseup', onStopSlide)
+  }
+  const handleValueSubmit = (newValue: string, isMinValue: boolean) => {
+    const value = Math.round(Number(newValue))
+    if (isMinValue) {
+      if (value >= initialMin && value < maxValue) setMinValue(value)
+      setEditingMinvalue(false)
+    } else {
+      if (value > minValue && value <= initialMax) setMaxValue(value)
+      setEditingMaxValue(false)
+    }
+  }
   return {
     minValue,
-    maxValue
+    maxValue,
+    isEditingMinValue,
+    isEditingMaxValue,
+    handleSlidingValue,
+    handleValueSubmit,
+    setEditingMinvalue,
+    setEditingMaxValue
   }
 }
